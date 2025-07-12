b5925aef4ff872eea7148b76d7a15528fc9cc943
Solomon yusuf <olafire03@gmail.com>
2025-07-12 22:26:40 +0100

    Added Feature for notification model

---
+++ Cancoly.Domain/Entities/Notification.cs
@@ -0,0 +1,21 @@
+﻿using Cancoly.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cancoly.Domain.Entities
+{
+    public class Notification : BaseEntity
+    {    
+        public string Title { get; set; }
+        public string Message { get; set; }
+        [MaxLength(300000)]
+        public string? MailBody { get; set; }
+        public bool AllowMail { get; set; } = true;
+        public bool MailSent { get; set; } = false;
+    }
+}
---
+++ Cancoly.Persistence/Repository/NotificationRepository.cs
@@ -0,0 +1,19 @@
+﻿using Cancoly.Application.IRepository;
+using Cancoly.Domain.Common;
+using Cancoly.Domain.Entities;
+using Cancoly.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cancoly.Persistence.Repository
+{
+    public class NotificationRepository : BaseRepository<Notification>, INotificationRepository
+    {
+        public NotificationRepository(MainDbContext context) : base(context)
+        {
+        }
+    }
+}
